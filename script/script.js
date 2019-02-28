f5d95054b68e13e3f94c2a12b3ea3a64689a51b7
Mattiasr <r.o.n.g.e@hotmail.com>
2019-02-28 13:28:01 +0100

    reverted a thing

--- script/script.js
+++ script/script.js
@@ -1,15 +1,10 @@
-var deck = [];
-var text = "Pedro"
-// Hämtar data ifrån JSON och skriver ut det i en array
+var deck;
 
 fetch("../JSON/cards.json").then(response => {
     response.json().then(json => {
         let data = json;
         deck = data;
-        shuffla(deck);
-        console.log(deck);
 
-// Loopar igenom array och skriver ut korten 
             for(i=0; i < deck.length; i++) {
 
                 var stuff = document.querySelector('div');
@@ -21,15 +16,6 @@ fetch("../JSON/cards.json").then(response => {
             }
     })
 });
-
-
-function shuffla (array) {
-    for (var a = array.length - 1,b, rand; a > 0; a--){
-        rand = Math.floor(Math.random() * (a + 1));
-        b = array[a], array[a] = array[rand], array[rand] = b;
-    }
-    return array;
-}
 
 
 
