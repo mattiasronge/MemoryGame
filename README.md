c5f23de4f7383caefec3c65937df6bc2c27446aa
Mattiasr <r.o.n.g.e@hotmail.com>
2019-03-04 13:55:46 +0100

    Har fixat sa att bilder kan matchas och att dem vander nar vardet inte ar samma, lite mer css, stadat koden lite.

--- README.md
+++ README.md
@@ -2,20 +2,4 @@
 Labb2 i JavaScript kursen
 
 
-### Att göra:
-(X) Skapa fil struktur och skapa mappar. 
-
-(X) Skapa Json fil som innehåller alla kort i spelet
-
-(X) En funktion som shufflar alla korten
-
-(X) Skriva ut alla kort på sidan
-
-() Lägga clickevent på samtliga kort så att man kan jämföra värdet
-
-() Göra så att man inte kan jämföra korten med sig själv
-
-() När två kort har samma värde så försvinner korten.
-
-() Skapa poängsystem som visar resultatet när alla är matchade och "spela om knapp"
 
--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 var deck = [];
+var cards = document.getElementsByTagName('img');
+var firstElem = null;
+var firstValue = null;
 
 // Hämtar data ifrån JSON och skriver ut det i en array
 
@@ -17,49 +20,70 @@ fetch("../JSON/cards.json").then(response => {
                 var imgBox = document.createElement("div");
                 var img = document.createElement("img");
                 img.src = deck[index].image;
-                img.classList = "hiddenImg"
-                imgBox.classList = "imgBox";
+                img.classList = "hiddenImg";
+                img.classList.add(index);
+                imgBox.classList.add("imgBox");
                 img.setAttribute("id", deck[index].value);
                 imgBox.appendChild(img);
                 stuff.appendChild(imgBox);
-                var firstElem = null;
-                var firstValue = null;
-       
-    
-                img.addEventListener('click',  (e) => {
-            
-             
-                    var target = e.target || e.srcElement;
-                    img.classList.remove("hiddenImg");
-                    img.classList.add("shownImg");
-    
-                    if (firstValue === null){
 
-                        firstElem = target;
-                        firstValue = target.id;
-                        console.log(firstValue);
-
-                    } else if (firstValue === target.id && target !== firstElem) {
-
-                        firstValue = null;
-                        
-                        imgBox.classList.add("noImg");
-                        imgBox.classList.remove("imgBox");
-            
-                        console.log("They the same yo");
-                        console.log(firstValue);
-
-                    } else {
-
-                        firstValue = null;
-                        console.log(firstValue);
-                   
-                    }
-                });
         })
+
+        for (var i = 0; i < cards.length; i++) {
+
+            cards[i].addEventListener('click',(e) => {
+
+                var target = e.target || e.srcElement;
+
+                setTimeout(() => {
+                        
+                    firstElem.classList.remove("hiddenImg");
+                    firstElem.classList.add("shownImg");
+
+                    target.classList.remove("hiddenImg");
+                    target.classList.add("shownImg");
+                    
+                }, 300);
+
+                if (firstValue === null){
+
+                    firstValue = target.id;
+                    firstElem = target;
+
+                    console.log("Card value is:",firstValue);
+                 
+
+                } else if (firstValue === target.id) {
+
+                    firstValue = null;
+
+                    console.log("Cards match!");    
+
+                    firstElem.classList.add("matchedCard");
+                    target.classList.add("matchedCard");
+                } 
+                else {
+
+                    firstValue = null;
+                    console.log("Card value is:", target.id);
+                    console.log("Cards dont match, turning cards now");
+
+                    setTimeout(() => {
+
+                        firstElem.classList.remove("shownImg");
+                        firstElem.classList.add("hiddenImg");
+
+                        target.classList.remove("shownImg");
+                        target.classList.add("hiddenImg");
+
+                    }, 800);
+                }
+            });
+        }
 
     })
 });
+
 
 function shuffla (array) {
     for (var a = array.length - 1,b, rand; a > 0; a--){
@@ -68,6 +92,4 @@ function shuffla (array) {
     }
     return array;
 }
-
-
 
